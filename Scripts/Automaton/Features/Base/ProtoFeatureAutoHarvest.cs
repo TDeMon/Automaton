f6abb4c47995290bbefabe6149db75b50242d9a3
Xobotun <me@xobotun.com>
2020-04-27 21:54:14 +0300

    Refactored target-finding code into another method. Still have troubles with nonobstructed targets. :/

--- Scripts/Automaton/Features/Base/ProtoFeatureAutoHarvest.cs
+++ Scripts/Automaton/Features/Base/ProtoFeatureAutoHarvest.cs
@@ -5,6 +5,7 @@
     using AtomicTorch.CBND.CoreMod;
     using AtomicTorch.CBND.CoreMod.Characters.Input;
     using AtomicTorch.CBND.CoreMod.Characters.Player;
+    using AtomicTorch.CBND.CoreMod.CraftRecipes;
     using AtomicTorch.CBND.CoreMod.Items.Weapons;
     using AtomicTorch.CBND.CoreMod.Systems.Physics;
     using AtomicTorch.CBND.CoreMod.Systems.Weapons;
@@ -58,79 +59,71 @@
             return true;
         }
 
-        private void FindAndAttackTarget( )
+        private IStaticWorldObject FindTarget(Vector2D weaponPos)
         {
-            var fromPos = CurrentCharacter.Position + GetWeaponOffset();
-            
+            double searchDistance = GetCurrentWeaponRange() * 25;
             using var objectsVisible = this.CurrentCharacter.PhysicsBody.PhysicsSpace
-                                          .TestCircle(position: fromPos,
-                                                      radius: this.GetCurrentWeaponRange() * 25, // I don't know what units the game uses, so let's stick with the search radius ~25 times as far as the weapon can hit.
+                                          .TestCircle(position: weaponPos,
+                                                      radius: searchDistance, // I don't know what units the game uses, so let's stick with the search radius ~25 times as far as the weapon can hit.
                                                       collisionGroup: CollisionGroups.HitboxMelee);
-                                          
+
             // I'd rather chain these methods, but it seems like `using` has some relation to IDisposable. I'm not sure of consequences not using it might cause, so I'll copy the code. It's not like I want to keep your memeory free from leaks or anything.
             var sortedVisibleObjects = objectsVisible?.AsList()
                                           ?.Where(t => this.EnabledEntityList.Contains(t.PhysicsBody?.AssociatedWorldObject?.ProtoGameObject))
                                           ?.Where(t => t.PhysicsBody?.AssociatedWorldObject is IStaticWorldObject)
                                           ?.Where(t => this.AdditionalValidation(t.PhysicsBody?.AssociatedWorldObject as IStaticWorldObject))
-                                          ?.OrderBy(obj => obj.PhysicsBody.Position.DistanceTo(fromPos))
+                                          //?.Where(t => this.CheckForObstacles(t.PhysicsBody.AssociatedWorldObject, weaponPos, searchDistance))
+                                          ?.OrderBy(obj => obj.PhysicsBody.Position.DistanceTo(weaponPos))
                                           ?.ToList();
             if (sortedVisibleObjects == null || sortedVisibleObjects.Count == 0)
             {
+                return null;
+            }
+
+            return sortedVisibleObjects[0].PhysicsBody.AssociatedWorldObject as IStaticWorldObject; // checked somewhere in the stream above
+        }
+
+        private void FindAndAttackTarget( )
+        {
+            var fromPos = CurrentCharacter.Position + GetWeaponOffset();
+
+            IStaticWorldObject target = FindTarget(fromPos);
+            if (target == null)
+            {
                 return;
             }
 
-            bool canAlreadyHit = sortedVisibleObjects[0].PhysicsBody.Position.DistanceTo(fromPos) < this.GetCurrentWeaponRange() / 2; // Get closer than maximal distance
+            bool canAlreadyHit = target.PhysicsBody.Position.DistanceTo(fromPos) < this.GetCurrentWeaponRange() / 2; // Get closer than maximal distance
             if (!canAlreadyHit)
             {
-                MoveToClosestTarget(fromPos, sortedVisibleObjects[0].PhysicsBody.AssociatedWorldObject);
+                MoveToClosestTarget(fromPos, target);
                 return; // Can safely ignore code below, because if we have to move to the closest object to attack it, we definitely cannot hit it.
             }
 
-            // Better to leave this code as is and not to link `objectOfInterest` to `sortedVisibleObjects`. There is a foreach loop below over a _single_ object, so that may mean there can be several decoratie objects within one PhysicsBody. Probably. Or two trees standing nearby.
-            using var objectsNearby = this.CurrentCharacter.PhysicsBody.PhysicsSpace
-                                          .TestCircle(position: fromPos,
-                                                      radius: this.GetCurrentWeaponRange(),
-                                                      collisionGroup: CollisionGroups.HitboxMelee);
 
-            var objectOfInterest = objectsNearby.AsList()
-                                   ?.Where(t => this.EnabledEntityList.Contains(t.PhysicsBody?.AssociatedWorldObject?.ProtoGameObject))
-                                   .ToList();
-            if (objectOfInterest == null || objectOfInterest.Count == 0)
+            var shape = target.PhysicsBody.Shapes.FirstOrDefault(s => s.CollisionGroup == CollisionGroups.HitboxMelee);
+            if (shape == null)
             {
+                Api.Logger.Error("Automaton: target object has no HitBoxMelee shape " + target);
                 return;
             }
-
-            foreach (var obj in objectOfInterest)
+            var targetPoint = this.ShapeCenter(shape) + target.PhysicsBody.Position;
+            if (this.CheckForObstacles(target, targetPoint, GetCurrentWeaponRange()))
             {
-                var testWorldObject = obj.PhysicsBody.AssociatedWorldObject as IStaticWorldObject;
-                var shape = obj.PhysicsBody.Shapes.FirstOrDefault(s =>
-                                                                      s.CollisionGroup == CollisionGroups.HitboxMelee);
-                if (shape == null)
-                {
-                    Api.Logger.Error("Automaton: target object has no HitBoxMelee shape " + testWorldObject);
-                    continue;
-                }
-                if(!this.AdditionalValidation(testWorldObject))
-                {
-                    continue;
-                }
-                var targetPoint = this.ShapeCenter(shape) + obj.PhysicsBody.Position;
-                if (this.CheckForObstacles(testWorldObject, targetPoint))
-                {
-                    this.AttackTarget(testWorldObject, targetPoint);
-                    this.attackInProgress = true;
-                    ClientTimersSystem.AddAction(this.GetCurrentWeaponAttackDelay(), () =>
-                                                                                     {
-                                                                                         if (this.attackInProgress)
-                                                                                         {
-                                                                                             this.attackInProgress = false;
-                                                                                             this.StopItemUse();
-                                                                                             this.FindAndAttackTarget();
-                                                                                         }
-                                                                                     });
-                    return;
-                }
+                this.AttackTarget(target, targetPoint);
+                this.attackInProgress = true;
+                ClientTimersSystem.AddAction(this.GetCurrentWeaponAttackDelay(), () =>
+                                                                                    {
+                                                                                        if (this.attackInProgress)
+                                                                                        {
+                                                                                            this.attackInProgress = false;
+                                                                                            this.StopItemUse();
+                                                                                            this.FindAndAttackTarget();
+                                                                                        }
+                                                                                    });
+                return;
             }
+          
         }
 
         public void AttackTarget(IWorldObject targetObject, Vector2D intersectionPoint)
@@ -185,12 +178,13 @@
             return toolItem?.FireInterval ?? 0d;
         }
 
-        private bool CheckForObstacles(IWorldObject targetObject, Vector2D intersectionPoint)
+        // Returns true if not obscured
+        private bool CheckForObstacles(IWorldObject targetObject, Vector2D intersectionPoint, double maxDistance)
         {
             // Check for obstacles in line between character and object
             var fromPos = CurrentCharacter.Position + GetWeaponOffset();
             // Normalize vector and set it length to weapon range
-            var toPos = (fromPos - intersectionPoint).Normalized * GetCurrentWeaponRange();
+            var toPos = (fromPos - intersectionPoint).Normalized * maxDistance;
             // Check if in range
             bool canReachObject = false;
             using var obstaclesOnTheWay = this.CurrentCharacter.PhysicsBody.PhysicsSpace
